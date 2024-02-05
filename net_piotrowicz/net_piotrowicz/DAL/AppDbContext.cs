3a5081ab66c7526684d1415017b062991a08dcd5
ALICJA\alicj <alicjapiotrowicz123@gmail.com>
2024-02-05 20:21:03 +0100

    products, DAL, DbContext, installed EntityFrameworkCore : design, sqlServer, tool

---
+++ net_piotrowicz/net_piotrowicz/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net_piotrowicz.Models;
+
+namespace net_piotrowicz.DAL
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { 
+        
+        }
+
+        public virtual DbSet <Product> Products { get; set; }
+    }
+}
